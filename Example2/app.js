0ab709ec62baa5541a7062ab079a8236c8c01c5a
Nat34 <nathan.a.thomas@gmail.com>
2016-04-13 20:51:19 -0400

    add notes

--- Example2/app.js
+++ Example2/app.js
@@ -9,8 +9,17 @@ var main = function () {
 $(document).ready(main);
 
 /* 
-Interactivity
+Interactivity - Handling click events
 
+* action: attaching an **event listener** to the DOM element (.comment-input button) referenced in the call to the function
+* Jquery: target elements in the DOM using CSS selectors
+* JavaScript: manipulate elements in the DOM
+* **When the user clicks the +, print "Hello, World!" to the console**
+* When the user [event for which we are listening on], then [function] occurs. event then function
+* the user does something and the page reacts: inter**activity**
 
-
+Things:
+event listener
+$ function
+appends
 */
