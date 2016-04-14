109fd8f3c58ad9a4f78faaa9916de80d61a4c268
Nat34 <nathan.a.thomas@gmail.com>
2016-04-13 22:21:39 -0400

    creating new paragraph element, then text content of the paragraph element is changing

--- Example2/app.js
+++ Example2/app.js
@@ -2,14 +2,24 @@ var main = function () {
 	"use strict";
 
 	$(".comment-input button").on("click", function (event) {
-		var $new_comment = $("<p>"); 
+		var $new_comment = $("<p>");
+			comment_text = $(".comment-input input").val();
 
-		$new_comment.text("this is a new comment");
-		$(".comments").append("<p>this is a new comment</p>");
+		$new_comment.text(comment text);
+		
 	});
 };
 
 $(document).ready(main);
+
+/* 
+
+* variable declaration and assignment
+* variable name $new_comment
+* <p> paragraph element as JQuery object 
+* Review: new paragraph element created then text content of the <p> is being changed to "this is a new comment")
+
+*/
 
 /* 
 Interactivity - Handling click events
@@ -28,13 +38,18 @@ appends
 .on
 .append
 .text
+refactor
 
 Removed:
 console.log("Hello, World!");
+$(".comments").append("<p>this is a new comment</p>");
 */
 
 /* Interactivity - Dynamically manipulating the DOM 
 
 * create a variable
+* variable declartion and assignment
+* variable name: *$new_comment* can be anything; but if the variable will hold a Jquery object use the `$` as the first character
+* 
 */
 
